4e2b62bdd6125e33820a963725a51929cc30253b
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-07-08 13:36:03 +0200

    first commit

---
+++ src/main/java/com/myfastfoodapp/FastFoodVoucherw/FastFoodVoucherwApplication.java
@@ -0,0 +1,13 @@
+package com.myfastfoodapp.FastFoodVoucherw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodVoucherwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodVoucherwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfastfoodapp/FastFoodVoucherw/FastFoodVoucherwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfastfoodapp.FastFoodVoucherw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodVoucherwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
