99fb48eafe9166dcc1da446b0f2d4f1d90d500da
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-07-09 17:18:28 +0200

    Added attributes for User class on model package

--- src/main/java/com/myfastfoodapp/FastFoodVoucherw/model/User.java
+++ src/main/java/com/myfastfoodapp/FastFoodVoucherw/model/User.java
@@ -1,5 +1,27 @@
 package com.myfastfoodapp.FastFoodVoucherw.model;
 
+import org.hibernate.mapping.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class User {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Double walletBalance;
+
+    @OneToMany(mappedBy = "user")
+    private List<Voucher> vouchers;
+
+    @OneToMany(mappedBy = "user")
+    private List<Transaction> transaction;
+
     
 }
