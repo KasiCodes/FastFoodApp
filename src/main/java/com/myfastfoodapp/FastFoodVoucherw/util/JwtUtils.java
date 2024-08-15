b319f53f0aff88e0dd4a48a8e04cceaf4efe7d7d
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-08-15 16:40:02 +0200

    added util methods

--- src/main/java/com/myfastfoodapp/FastFoodVoucherw/util/JwtUtils.java
+++ src/main/java/com/myfastfoodapp/FastFoodVoucherw/util/JwtUtils.java
@@ -1,5 +1,43 @@
 package com.myfastfoodapp.FastFoodVoucherw.util;
 
-public class JwtUtils {
-    
-}
+   import io.jsonwebtoken.Claims;
+   import io.jsonwebtoken.Jwts;
+   import io.jsonwebtoken.SignatureAlgorithm;
+   import org.springframework.stereotype.Component;
+
+   import java.util.Date;
+
+   @Component
+   public class JwtUtils {
+
+   	private final String jwtSecret = "your_secret_key";  // Replace with a more secure key
+
+   	private final int jwtExpirationMs = 86400000;  // 1 day
+
+   	public String generateJwtToken(String username) {
+       	return Jwts.builder()
+               	.setSubject(username)
+               	.setIssuedAt(new Date())
+               	.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+               	.signWith(SignatureAlgorithm.HS512, jwtSecret)
+               	.compact();
+   	}
+
+   	public String getUserNameFromJwtToken(String token) {
+       	Claims claims = Jwts.parser()
+               	.setSigningKey(jwtSecret)
+               	.parseClaimsJws(token)
+               	.getBody();
+       	return claims.getSubject();
+   	}
+
+   	public boolean validateJwtToken(String token) {
+       	try {
+           	Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+           	return true;
+       	} catch (Exception e) {
+           	// Token is invalid
+       	}
+       	return false;
+   	}
+   }
